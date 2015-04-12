dcdfde5bbde7f01cfcb92a40cc2e74fd01f0b249
Dan Kang <dan@dskang.com>
2015-04-12 14:05:44 -0400

    Add updateFriends to add list of friends

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,56 @@ var utils = require("cloud/utils.js");
 // Max distance between nearby users
 var NEARBY_DISTANCE = 150;
 
+Parse.Cloud.define("updateFriends", function(request, response) {
+  var user = request.user;
+  if (!user) {
+    response.error("Request does not have an associated user.");
+    return;
+  }
+
+  var authData = user.get("authData");
+  var accessToken = authData["facebook"]["access_token"];
+
+  // Get Facebook friends
+  Parse.Cloud.httpRequest({
+    url: "https://graph.facebook.com/me/friends",
+    params: {
+      access_token: accessToken,
+      limit: 5000 // get all friends in one request
+    },
+    success: function(httpResponse) {
+      var friends = httpResponse.data.data;
+      var friendFbIds = friends.map(function(friend) { return friend.id });
+      var friendQuery = new Parse.Query(Parse.User);
+      friendQuery.containedIn("fbId", friendFbIds);
+      friendQuery.find({
+        success: function(results) {
+          var relation = user.relation("friends");
+          for (var i = 0; i < results.length; i++) {
+            var friend = results[i];
+            relation.add(friend);
+          }
+          user.save(null, {
+            success: function(user) {
+              response.success();
+            },
+            error: function(user, error) {
+              response.error("Error: " + error.code + " " + error.message);
+            }
+          });
+        },
+        error: function(error) {
+          response.error("Error: " + error.code + " " + error.message);
+        }
+      });
+    },
+    error: function(httpResponse) {
+      var error = httpResponse.data.error;
+      response.error(error.type + " (" + error.code + "): " + error.message);
+    }
+  });
+});
+
 Parse.Cloud.define("nearbyFriends", function(request, response) {
   var user = request.user;
   if (!user) {
@@ -65,6 +115,7 @@ Parse.Cloud.define("wave", function(request, response) {
   var recipientId = request.params.recipientId;
 
   // TODO: Validate that sender is allowed to send to recipient (friend, nearby)
+  // TODO: Check if recipient is in stealth mode
 
   var recipient = new Parse.User();
   recipient.id = recipientId;
