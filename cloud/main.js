7fbbefeeecba8847eaa5475e1fc333510e23bb70
Dan Kang <dan@dskang.com>
2015-04-12 14:12:03 -0400

    Update nearbyFriends to use friends relation

--- cloud/main.js
+++ cloud/main.js
@@ -65,42 +65,24 @@ Parse.Cloud.define("nearbyFriends", function(request, response) {
     return;
   }
 
-  var authData = user.get("authData");
-  var accessToken = authData["facebook"]["access_token"];
-  // Get Facebook friends
-  Parse.Cloud.httpRequest({
-    url: "https://graph.facebook.com/me/friends",
-    params: {
-      access_token: accessToken,
-      limit: 5000 // get all friends in one request
-    },
-    success: function(httpResponse) {
-      var friends = httpResponse.data.data;
-      var friendIDs = friends.map(function(friend) { return friend.id });
-      var friendQuery = new Parse.Query(Parse.User);
-      friendQuery.select("location", "name");
-      friendQuery.containedIn("fbId", friendIDs);
-      friendQuery.notEqualTo("hideLocation", true);
-      friendQuery.find({
-        success: function(results) {
-          var nearbyFriends = results.filter(function(friend) {
-            var userLocation = user.get("location");
-            var friendLocation = friend.get("location");
-            var distance = utils.haversineDistance(
-              userLocation.latitude, userLocation.longitude,
-              friendLocation.latitude, friendLocation.longitude);
-            return distance <= NEARBY_DISTANCE;
-          });
-          response.success(nearbyFriends);
-        },
-        error: function(error) {
-          response.error("Error: " + error.code + " " + error.message);
-        }
+  var relation = user.relation("friends");
+  var friendQuery = relation.query();
+  friendQuery.select("location", "name");
+  friendQuery.notEqualTo("hideLocation", true);
+  friendQuery.find({
+    success: function(results) {
+      var nearbyFriends = results.filter(function(friend) {
+        var userLocation = user.get("location");
+        var friendLocation = friend.get("location");
+        var distance = utils.haversineDistance(
+          userLocation.latitude, userLocation.longitude,
+          friendLocation.latitude, friendLocation.longitude);
+        return distance <= NEARBY_DISTANCE;
       });
+      response.success(nearbyFriends);
     },
-    error: function(httpResponse) {
-      var error = httpResponse.data.error;
-      response.error(error.type + " (" + error.code + "): " + error.message);
+    error: function(error) {
+      response.error("Error: " + error.code + " " + error.message);
     }
   });
 });
