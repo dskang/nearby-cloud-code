6b20787c8ef164e5842b98d6b789e79ea31293ee
Dan Kang <dan@dskang.com>
2015-04-28 18:27:12 -0400

    Rewrote updateFriends to use promises

--- cloud/main.js
+++ cloud/main.js
@@ -50,65 +50,63 @@ var hasBlocked = function(user1, user2) {
 Parse.Cloud.define("updateFriends", function(request, response) {
   Parse.Cloud.useMasterKey();
   var user = request.user;
+  updateFriends(user).then(function() {
+    response.success();
+  }, function(error) {
+    var message;
+    if (error.type && error.code && error.message) {
+      // Facebook error
+      message = error.type + " (" + error.code + "): " + error.message;
+    } else if (error.code && error.message) {
+      // Parse error
+      message = "(" + error.code + "): " + error.message;
+    } else {
+      message = error;
+    }
+    response.error(message);
+  });
+});
+
+var updateFriends = function(user) {
   if (!user) {
-    response.error("Request does not have an associated user.");
-    return;
+    return Parse.Promise.error("Request does not have an associated user.");
   }
 
   var authData = user.get("authData");
   var accessToken = authData["facebook"]["access_token"];
 
   // Get Facebook friends
-  Parse.Cloud.httpRequest({
+  return Parse.Cloud.httpRequest({
     url: "https://graph.facebook.com/me/friends",
     params: {
       access_token: accessToken,
       limit: 5000 // get all friends in one request
-    },
-    success: function(httpResponse) {
-      var friends = httpResponse.data.data;
-      var friendFbIds = friends.map(function(friend) { return friend.id });
-      var friendQuery = new Parse.Query(Parse.User);
-      friendQuery.containedIn("fbId", friendFbIds);
-      friendQuery.find({
-        success: function(friends) {
-          var relation = user.relation("friends");
-          for (var i = 0; i < friends.length; i++) {
-            // Add friend as user's friend
-            var friend = friends[i];
-            relation.add(friend);
-
-            // Add user as friend's friend
-            var friendRelation = friend.relation("friends");
-            friendRelation.add(user);
-          }
-          Parse.Object.saveAll(friends, {
-            success: function(list) {
-              user.save(null, {
-                success: function(user) {
-                  response.success();
-                },
-                error: function(user, error) {
-                  response.error("Error: " + error.code + " " + error.message);
-                }
-              });
-            },
-            error: function(error) {
-              response.error("Error: " + error.code + " " + error.message);
-            }
-          });
-        },
-        error: function(error) {
-          response.error("Error: " + error.code + " " + error.message);
-        }
-      });
-    },
-    error: function(httpResponse) {
-      var error = httpResponse.data.error;
-      response.error(error.type + " (" + error.code + "): " + error.message);
     }
+  }).then(function(httpResponse) {
+    var friends = httpResponse.data.data;
+    var friendFbIds = friends.map(function(friend) { return friend.id });
+    var friendQuery = new Parse.Query(Parse.User);
+    friendQuery.containedIn("fbId", friendFbIds);
+    return friendQuery.find();
+  }, function(httpResponse) {
+    var error = httpResponse.data.error;
+    return Parse.Promise.error(error);
+  }).then(function(friends) {
+    var relation = user.relation("friends");
+    for (var i = 0; i < friends.length; i++) {
+      // Add friend as user's friend
+      var friend = friends[i];
+      relation.add(friend);
+
+      // Add user as friend's friend
+      var friendRelation = friend.relation("friends");
+      friendRelation.add(user);
+    }
+    var saveFriends = Parse.Object.saveAll(friends);
+    var saveUser = user.save();
+    return Parse.Promise.when([saveFriends, saveUser]);
   });
-});
+};
 
 var getNearbyFriendsQuery = function(user) {
   var relation = user.relation("friends");
